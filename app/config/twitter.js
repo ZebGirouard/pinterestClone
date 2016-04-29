fb9ee66d666c366c747d5f0d25893a9163e1e466
Timothy Zeb Girouard <timzebgir@gmail.com>
2016-04-29 23:51:11 +0000

    Adjusting Twitter auth callback url.

--- app/config/twitter.js
+++ app/config/twitter.js
@@ -4,5 +4,5 @@
 module.exports = {
 	'apikey' : 'Chf8rsRiVslMBRwMR6wKFBMFO',
 	'apisecret' : 'YAe5A9rRmwn72w193CPw65WIpNAIk8Ak21KaTl7zinxaDCoLNu',
-	'callbackUrl' : 'https://clementine-projects-zebgirouard.c9users.io/login/twitter/callback'
+	'callbackUrl' : 'https://afternoon-everglades-39990.herokuapp.com/login/twitter/callback'
 };
